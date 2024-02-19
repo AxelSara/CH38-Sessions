2bd3cb90ffe9576ab1e76fd0bc06d4c3d2632239
Axel Sara <axelsaragar@gmail.com>
2024-02-19 13:26:00 -0600

    typo: add a button with a prompt

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello CH38");
+/*
+ * Funcion con alert
+ */
+function alertPage(){
+    alert("idk, just an alert");
+}
+/*
+ * Ask the person's name using prompt
+ * Show the person's name with alert
+ */
+function sentWavePerson(){
+    let name = prompt("Write your name");
+    alert("Hi " + name);
+}
